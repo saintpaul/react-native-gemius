fbdeaae54e6b716eb36321e202953773bf5430c3
vandervdb <vanderbeta-1@yahoo.fr>
2020-03-31 15:42:12 +0200

    bump package version

--- ios/RNReactNativeGemius.m
+++ ios/RNReactNativeGemius.m
@@ -9,7 +9,7 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
     // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    callback(@[[NSString stringWithFormat: @"NumberArgument: %@ StringArgument: %@", numberArgument, stringArgument]]);
 }
 
 RCT_EXPORT_METHOD(setAppInfo:(NSString *)app version:(NSString *)version)
