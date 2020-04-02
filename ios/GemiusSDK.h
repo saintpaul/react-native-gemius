32608f3e541f7581166acfae80c785df9878a270
vandervdb <vanderbeta-1@yahoo.fr>
2020-04-02 13:59:56 +0200

    Move gemius classes from framework dir to proj dir

---
+++ ios/GemiusSDK.h
@@ -0,0 +1,34 @@
+//
+//  GemiusSDK.h
+//  GemiusSDK
+//
+//  Copyright © 2018 Gemius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GemiusSDK.
+FOUNDATION_EXPORT double GemiusSDKVersionNumber;
+
+//! Project version string for GemiusSDK.
+FOUNDATION_EXPORT const unsigned char GemiusSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PublicHeader.h>
+
+#import <Config.h>
+#import <AdOceanConfig.h>
+#import <AdDefine.h>
+#import <GEMBillboardAd.h>
+#import <GEMFullScreenAd.h>
+#import <AudienceEvent.h>
+#import <NetpanelEvent.h>
+#import <AudienceConfig.h>
+#import <NetpanelConfig.h>
+#import <ContentData.h>
+#import <AdData.h>
+#import <ProgramData.h>
+#import <EventData.h>
+#import <EventProgramData.h>
+#import <EventAdData.h>
+#import <PlayerData.h>
+#import <Player.h>
